959e5ddb4a98711bb1ffe162b550416b71289cf0
SerinaNya <34389622+SerinaNya@users.noreply.github.com>
2025-08-17 14:25:38 +0800

    ⚡️ perf(httpx_requests): add any_api_base

--- utils/httpx_requests.py
+++ utils/httpx_requests.py
@@ -1,8 +1,12 @@
+from typing import Any
 from utils.config_manager import config
 
-littleskin_api_base = {
-    "headers": config.littleskin.auth_header,
-    "base_url": config.littleskin.endpoint,
+any_api_base: dict[str, Any] = {
     "follow_redirects": True,
     "http2": True,
+}
+
+littleskin_api_base = any_api_base | {
+    "headers": config.littleskin.auth_header,
+    "base_url": config.littleskin.endpoint,
 }
