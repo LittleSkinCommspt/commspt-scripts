ad3e4c611b1ad81e2cf2e68468d8e790a1656860
SerinaNya <34389622+SerinaNya@users.noreply.github.com>
2026-01-14 17:35:21 +0800

    fix: update datetime_start to reflect correct time range and add assertions for log responses

--- oci_email_statistics.py
+++ oci_email_statistics.py
@@ -14,11 +14,10 @@ client = LogSearchClient(config)
 
 compartment_id = config["tenancy"]
 
-
+# time range
 timezone = ZoneInfo("Asia/Shanghai")
 datetime_end = datetime.now(timezone).replace(hour=5, minute=0, second=0, microsecond=0)  # Today 05:00:00
-datetime_start = datetime_end - timedelta(days=1)  # Yesterday 00:00:00
-
+datetime_start = datetime_end - timedelta(days=1)  # Yesterday 05:00:00
 
 log_stream_accepted = f"{compartment_id}/{CONFIG.oci.log_group_id}/{CONFIG.oci.log_object_id_accepted}"
 log_stream_relayed = f"{compartment_id}/{CONFIG.oci.log_group_id}/{CONFIG.oci.log_object_id_relayed}"
@@ -47,13 +46,20 @@ resp_accepted_suppressed = client.search_logs(details_accepted_suppressed)
 resp_relayed_total = client.search_logs(details_relayed_total)
 resp_relayed_bounced = client.search_logs(details_relayed_bounced)
 
+# assert
+assert resp_accepted_total, "Failed to get accepted total"
+assert resp_accepted_suppressed, "Failed to get accepted suppressed"
+assert resp_relayed_total, "Failed to get relayed total"
+assert resp_relayed_bounced, "Failed to get relayed bounced"
+
 accepted_total = resp_accepted_total.data.results[0].data["count"]
 accepted_suppressed = resp_accepted_suppressed.data.results[0].data["count"]
 relayed_total = resp_relayed_total.data.results[0].data["count"]
 relayed_bounced = resp_relayed_bounced.data.results[0].data["count"]
 
-
-message = f"""📊 OCI Email 统计 {datetime_start.strftime("%m-%d")} ~ {datetime_end.strftime("%m-%d")}
+# send message
+message = f"""📊 OCI Email 统计 
+--- {datetime_start.strftime("%m-%d %H:%M")} ~ {datetime_end.strftime("%m-%d %H:%M")} ---
 
 Accepted Total: {accepted_total}
 Accepted Suppressed: {accepted_suppressed}
