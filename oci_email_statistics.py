59c9b9164d76c6e52475ef0b9d7995384c08b23b
SerinaNya <34389622+SerinaNya@users.noreply.github.com>
2026-01-16 23:26:51 +0800

    feat: add recipient statistics to OCI email logs

--- oci_email_statistics.py
+++ oci_email_statistics.py
@@ -25,9 +25,13 @@ log_stream_relayed = f"{compartment_id}/{config.oci.log_group_id}/{config.oci.lo
 details_accepted_total = SearchLogsDetails(
     search_query=f'search "{log_stream_accepted}" | count', time_start=datetime_start, time_end=datetime_end
 )
-
 details_accepted_suppressed = SearchLogsDetails(
     search_query=f"search \"{log_stream_accepted}\" | where data.errorType='Recipient suppressed' | count",
+    time_start=datetime_start,
+    time_end=datetime_end,
+)
+details_accepted_recipients = SearchLogsDetails(
+    search_query=f'search "{log_stream_accepted}" | summarize count() by data.recipient | count',
     time_start=datetime_start,
     time_end=datetime_end,
 )
@@ -40,22 +44,34 @@ details_relayed_bounced = SearchLogsDetails(
     time_start=datetime_start,
     time_end=datetime_end,
 )
+details_relayed_recipients = SearchLogsDetails(
+    search_query=f'search "{log_stream_relayed}" | summarize count() by data.recipient | count',
+    time_start=datetime_start,
+    time_end=datetime_end,
+)
+
 
 resp_accepted_total = client.search_logs(details_accepted_total)
 resp_accepted_suppressed = client.search_logs(details_accepted_suppressed)
+resp_accepted_recipients = client.search_logs(details_accepted_recipients)
 resp_relayed_total = client.search_logs(details_relayed_total)
 resp_relayed_bounced = client.search_logs(details_relayed_bounced)
+resp_relayed_recipients = client.search_logs(details_relayed_recipients)
 
 # assert
 assert resp_accepted_total, "Failed to get accepted total"
 assert resp_accepted_suppressed, "Failed to get accepted suppressed"
+assert resp_accepted_recipients, "Failed to get accepted recipients"
 assert resp_relayed_total, "Failed to get relayed total"
 assert resp_relayed_bounced, "Failed to get relayed bounced"
+assert resp_relayed_recipients, "Failed to get relayed recipients"
 
 accepted_total = resp_accepted_total.data.results[0].data["count"]
 accepted_suppressed = resp_accepted_suppressed.data.results[0].data["count"]
+accepted_recipients = resp_accepted_recipients.data.results[0].data["count"]
 relayed_total = resp_relayed_total.data.results[0].data["count"]
 relayed_bounced = resp_relayed_bounced.data.results[0].data["count"]
+relayed_recipients = resp_relayed_recipients.data.results[0].data["count"]
 
 # send message
 message = f"""📊 OCI Email 统计 
@@ -63,8 +79,13 @@ message = f"""📊 OCI Email 统计
 
 Accepted Total: {accepted_total}
 Accepted Suppressed: {accepted_suppressed}
+Accepted Recipients: {accepted_recipients}
+Suppressed Rate: {round(accepted_suppressed / accepted_total * 100, 2)}%
+
 Relayed Total: {relayed_total}
 Relayed Bounced: {relayed_bounced}
+Relayed Recipients: {relayed_recipients}
+Bounced Rate: {round(relayed_bounced / relayed_total * 100, 2)}%
 """
 
 print(message)
